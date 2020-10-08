05271dac8abaaf20c6822dfdc275549c0cca04b7
Sagor <mbrsagor@gmail.com>
2020-10-09 02:07:26 +0600

    Login page design and react-router added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import Login from './pages/Login';
 
 class App extends Component {
   render() {
@@ -10,6 +11,7 @@ class App extends Component {
           <div className="row">
             <div className="col-lg-12">
               <h2>API Integrat</h2>
+              <Login />
             </div>
           </div>
         </div>
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -4,7 +4,42 @@ class Login extends Component {
     state = {}
     render() {
         return (
-            <></>
+            <>
+                <div className="container pt-3">
+                    <div className="row justify-content-sm-center">
+                        <div className="col-sm-6 col-md-4">
+                            <div className="card border-info">
+                                <div className="card-header text-center"> Sign in to continue </div>
+                                <div className="card-body">
+                                    <div className="logo text-center">
+                                        <img className="rounded-circle" src="https://placeimg.com/128/128/tech/sepia" />
+                                    </div>
+                                    <form className="form-signin mt-3">
+                                        <div className="from-control">
+                                            <label htmlFor="usenrame">Username</label>
+                                            <input type="text"
+                                                className="form-control mb-2"
+                                                placeholder="Email" required autofocus
+                                            />
+                                        </div>
+                                        <div className="from-control">
+                                            <label htmlFor="usenrame">Password</label>
+                                            <input type="password"
+                                                className="form-control mb-2"
+                                                placeholder="Password" required
+                                            />
+                                        </div>
+                                        <button className="btn btn-sm btn-primary mr-5 mb-1" type="submit">Sign in</button>
+                                        <label className="checkbox">
+                                            <input type="checkbox" value="remember-me" /> Remember me </label>
+                                    </form>
+                                </div>
+                            </div>
+                            <a href="#0" className="float-right">Create an account </a>
+                        </div>
+                    </div>
+                </div>
+            </>
         );
     }
 }
